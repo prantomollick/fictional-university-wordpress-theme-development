57ebfca773a5a16248c8e3aa4a9ee71e24a56398
Pranto Mollick <pranto@example.com>
2025-01-26 16:14:15 +0600

    first filter test using my plugins

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -1,3 +1,22 @@
 <?php
 
-echo "Hello world";
+/*
+    Plugin Name: Our First Unique Plugin
+    Plugin URI: https://prantomollick.com
+    Description: This is our first unique plugin.
+    Version: 1.0
+    Author: Pranto Mollick
+    Author URI: https://prantomollick.com
+    License: GPLv2 or later
+    Text Domain: our-first-unique-plugin
+    Our First Unique Plugin is free software: you can redistribute it and/or modify
+*/
+
+function addToEndOfPost($content) {
+
+    if ( is_single() && is_main_query() ) {
+        return $content . '<p>Thank you for reading our first unique plugin!</p>';
+    }
+
+}
+add_filter('the_content', 'addToEndOfPost');
